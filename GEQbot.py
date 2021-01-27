a467c022d687009247e094815bed613902cd6661
Koh Jia Xian <39460640+koh-jx@users.noreply.github.com>
2021-01-28 00:29:14 +0800

    Updated GEQbot
    - Added gettype, gettext functions
    - Refined message saving system: 
    Uses a list instead of dictionary for messageList
    messageList holds a messageClass instead (Explained in comments)

--- GEQbot.py
+++ GEQbot.py
@@ -19,7 +19,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Dictionary of all user IDs
 userdict = {}
 #For conversationHandler
-MENU = range(1)
+ACTION, GETTITLE, GETTEXT = range(3)
 
 #Channel ID to forward messages to, bot must be Admin in the channel
 CHANNELID = -1001306746114
@@ -30,13 +30,36 @@ TOKEN = '1532597200:AAGGlFLs0VEPqnMLdfy7qh5jXFntuSUs8iI'
 
 #CLASS(ES)
 #===========================================================
+# Classes will be used in global dictionary userdict. To access an individual message's text:
+# userdict[userid].messageList[index].text
+
+
+# Message class contains the title and text of the message, so they can be retrieved from the database.
+# Each message also has a type: 1 - Donation/Exchange, 2 - Sharing, 3 - Looking for
+# Message has 2 mutators, to change the title and text as the user sees fit (For creation and editing purposes)
+class Message:
+    def __init__(self, type):
+        self.id = 0
+        self.type = type
+        self.title = ""
+        self.text = ""
+    
+    def set_title(self, title):
+        self.title = title
+    
+    def set_text(self, text):
+        self.text = text
+
+
+# The User class contains the name of the user, as well as a messageList,
+# with values of type Message. As the user makes/deletes offers, the messageList will be updated with his/her current messages.
 class User:
     def __init__(self, name):
         self.name = name
-        self.messageList = {}
+        self.messageList = []
     
-    def addMessage(self, msgid, title):
-        self.messageList[msgid] = title
+    def addMessage(self, type):
+        self.messageList.append(Message(type))
 
 
 
@@ -47,36 +70,73 @@ class User:
 def start(update, context) -> int:    
     name = update.message.from_user.first_name
     user_id = update.message.from_user.id
+
+    # Create user if not found in dictionary
     if user_id not in userdict.keys():
-        userdict[user_id] = User(name)                                          
-    reply_keyboard = [['Share/Donate', 'Look for items', 'Manage Posts']]
+        userdict[user_id] = User(name)
+
+    reply_keyboard = [['Make Post', 'Manage Posts']]
     reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     update.message.reply_text(
         """Hello {}!
 Got hostel essentials to share or donate? Looking for something in particular?
 Send /cancel to stop the bot UwU
-        """.format(name), reply_markup = reply_markup
-    )
+        """.format(name), reply_markup = reply_markup)
     
-    return MENU
+    return ACTION
 
-def looking(update: Update, context: CallbackContext) -> int:
+def newpost(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     option = update.message.text
-    print(option)
-    update.message.reply_text("Looking for items?")
+
+    print(user.first_name + " chose " + option)
+    reply_keyboard = [['Put on offer', 'Share item', 'Look for item']]
+    reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    update.message.reply_text("What are you looking to do today?", reply_markup = reply_markup)
+
+    return GETTITLE
+
+def gettype(option):
+    if (option == 'Put on offer'):
+        return 1
+    elif (option == 'Share item'):
+        return 2
+    elif (option == 'Look for item'):
+        return 3
+    else:
+        return 0
+
+def gettitle(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    option = update.message.text
+    userid = user.id
+    type = gettype(option)
+
+    # Create Message
+    userdict[userid].addMessage(type)
+
+    update.message.reply_text("Input the title of your post.")
+
+    return GETTEXT
+
+def gettext(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    title = update.message.text
+    userid = user.id
+
+    # Update title
+    userdict[userid].messageList[-1].set_title(title)
+
+    update.message.reply_text("Input the text of your post.")
+
+    #TESTING: TO CREATE A METHOD IN MESSAGE THAT COMPILES EVERYTHING AND RETURNS ONE SHOT
+    update.message.reply_text(userdict[userid].messageList[-1].type)
+    update.message.reply_text(userdict[userid].messageList[-1].title)
+
 
     return ConversationHandler.END
 
-def donating(update: Update, context: CallbackContext) -> int:
-    user = update.message.from_user
-    option = update.message.text
-    print(option)
-    update.message.reply_text("Sharing is Caring")
-
-    return ConversationHandler.END
-
-
+#Fallback function: Conversation cancelled at any time with /start
 def cancel(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     update.message.reply_text(
@@ -91,6 +151,15 @@ def cancel(update: Update, context: CallbackContext) -> int:
 
 #MAIN
 #===========================================================
+# Conversation gets your action first (Cancel at any time) [return  ACTION]
+#     Make Post? Get the type of your action    [return GETTITLE]
+#                     Get the title of your action [return GETTEXT]
+#                         Get the text of your action (and images if necessary) [return GETIMAGE]
+#                             Generate message, double-confirm
+#                                 Post in channel
+#     Manage Posts  .......
+
+
 def main():
     updater = Updater(token = TOKEN, use_context = True)
     dispatcher = updater.dispatcher
@@ -98,13 +167,9 @@ def main():
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            MENU: [MessageHandler(Filters.regex('Look for items'), looking), MessageHandler(Filters.regex('Share/Donate'), donating)],
-            # PHOTO: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
-            # LOCATION: [
-            #     MessageHandler(Filters.location, location),
-            #     CommandHandler('skip', skip_location),
-            # ],
-            # BIO: [MessageHandler(Filters.text & ~Filters.command, bio)],
+            ACTION: [MessageHandler(Filters.regex('Make Post'), newpost)],     # TO DO FOR MANAGE POSTS
+            GETTITLE: [MessageHandler(Filters.text, gettitle)],
+            GETTEXT: [MessageHandler(Filters.text, gettext)],
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     )
